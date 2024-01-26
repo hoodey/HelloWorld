a68efbb77ef53e460eeca9ddeca0bbf3a4ae14f1
hoodey <41493055+hoodey@users.noreply.github.com>
2024-01-26 11:42:25 -0600

    Hello World Script Update
    Updated my hello world script to include the functionality of printing hello world to console.

---
+++ HelloWorld/Assets/HelloWorld.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+
+public class HeeloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string phrase = "Hello World!";
+        Debug.Log(phrase);
+        int randomNum = Random.Range(1, 11);
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
